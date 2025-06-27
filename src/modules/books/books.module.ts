c567bebbdab7e3b0245ec53376e4767b8c9904de
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-06-27 20:32:15 +0300

    added regisrtration in books.module.ts

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { BooksController } from './books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
+import { BooksRepository } from '../books.repository';
+import { Book } from '../books.entity';
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController],
-  providers: [BooksService]
+  providers: [BooksService, BooksRepository],
 })
 export class BooksModule {}
